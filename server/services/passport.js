4c25037ba46be94f787324a2a4ad0c6742da3450
Ivan5 <converse_ivan@hotmail.com>
2019-02-28 15:28:34 -0600

    Save the doc in mongo

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,13 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL:'/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-
+    User.findOne({ googleId: profile.id})
+      .then((existingUser) => {
+        if (existingUser) {
+          //We already have a record
+          //pass a callback without any errors
+          done(null,existingUser);
+        }else{
+          //Make a new record
+          //save de new User in Db and pass to callback
+          new User({googleId:profile.id }).save()
+            .then(user => done(null,user))
+        }
+      })
   })
 );
