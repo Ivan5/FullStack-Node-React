e99c8b7981052f313f8594cf7b46d1511cf916a4
Ivan5 <converse_ivan@hotmail.com>
2019-02-28 17:27:53 -0600

    Implements cookies

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,27 +1,41 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL:'/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id})
-      .then((existingUser) => {
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           //We already have a record
           //pass a callback without any errors
-          done(null,existingUser);
-        }else{
+          done(null, existingUser);
+        } else {
           //Make a new record
           //save de new User in Db and pass to callback
-          new User({googleId:profile.id }).save()
-            .then(user => done(null,user))
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
         }
-      })
-  })
+      });
+    }
+  )
 );
