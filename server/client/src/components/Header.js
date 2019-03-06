00f2c0c7d5e30daab700b099fe2693953d4cc88c
Ivan5 <converse_ivan@hotmail.com>
2019-03-05 18:42:02 -0600

    Make the style of Header component

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div />;
+    return (
+      <nav>
+        <div className="nav-wrapper amber darken-1">
+          <a className="left brand-logo">Emaly</a>
+          <ul className="right">
+            <li>
+              <a>Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
